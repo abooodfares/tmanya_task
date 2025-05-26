e38b8aca85fa4592f9373764567d168a5c298315
abooodfares <abooodfaresali@gmail.com>
2025-05-26 18:13:42 +0300

    complete task

--- frontend/my-app/src/components/layout/Header.tsx
+++ frontend/my-app/src/components/layout/Header.tsx
@@ -3,8 +3,13 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 export const Header = (): JSX.Element => {
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <Toolbar>
-        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+      <Toolbar sx={{ justifyContent: 'flex-end' }}>
+        <Typography 
+          variant="h6" 
+          noWrap 
+          component="div" 
+          sx={{ flexGrow: 0, textAlign: 'right' }}
+        >
           بحث البودكاست
         </Typography>
       </Toolbar>
